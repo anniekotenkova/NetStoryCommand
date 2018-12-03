233384c4cb9c2391ab1f91c35952c3578fe1234d
anniekotenkova <31893553+anniekotenkova@users.noreply.github.com>
2018-12-03 17:42:32 +0300

    z-index to elements
    теперь перетаскиваемые  элементы переносятся на первый план

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,15 @@
 
 $(function() {
   $('.draggable').draggable();
+  $('.draggable').mousedown(function() //associate mousedown event with developments
+  {
+    var maxZindex = 0; // keep traking max z-index
+    $(this).siblings('.draggable').each(function() //find all the siblings of that development
+    {
+      var currentZindex = Number($(this).css('z-index')); //retrieve z-index of each development we are looping through
+      maxZindex = currentZindex > maxZindex ? currentZindex : maxZindex;
+    });
+    $(this).css('z-index', maxZindex + 1);
+  });
   $('.resizable').resizable();
 });
