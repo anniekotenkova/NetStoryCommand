9834d47213fb01280654ed7df14c5bee88b11692
hse <olya.shumilova@mail.ru>
2018-12-04 09:31:23 +0300

    open folder

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -78,4 +78,22 @@ var init = function(){
 	}
 }
 
+// Get element
+var folder = document.GetElementById('#folder');
+var folderfolder = document.GetElementById('#folderfolder');
+var close-btn = document.GetElementById('#close-btn')[0];
+
+// Events
+folderfolder.addEventListener('click', openFolder);
+close-btn.addEventListener('click', closeFolder);
+
+// Open
+function openFolder() {
+  folder.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  folder.style.display = 'none';
+}
 init();
