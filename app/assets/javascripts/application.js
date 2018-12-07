10655e2e631fe9284dbd35802e08c84f3560ea45
OlShum <olya.shumilova@mail.ru>
2018-12-07 11:31:33 +0300

    close windows

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,8 +58,19 @@ $( "#trashfolder" ).dblclick(function() {
 });
 
 
-$( "#close-btn" ).click(function() {
-  $('.foldercontent').css('display','none');
+$( ".cbinfo" ).click(function() {
+$('#info').css('display','none');
 });
+
+$( ".cbcomputer" ).click(function() {
+$('#computer').css('display','none');
+});
+
+$( ".cbfolder" ).click(function() {
+$('#folder').css('display','none');
+});
+
+$( ".cbtrash" ).click(function() {
+$('#trash').css('display','none');
 
 });
