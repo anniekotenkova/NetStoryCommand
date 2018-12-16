fe0a8b66fcf8ee5b3a7180820ba4d220011f7246
anniekotenkova <31893553+anniekotenkova@users.noreply.github.com>
2018-12-17 00:31:11 +0300

    Seems like welikolepno
    Все стили воедино! Все работает, радуемся

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //= require rails_sortable
 //= require_tree .
 
-//ARTICLES PREVIEW
+
 $(function() {
   $('.draggable').draggable();
   $('.draggable').mousedown(function() //associate mousedown event with developments
@@ -24,9 +24,7 @@ $(function() {
   });
 
   $('.resizable').resizable();
-});
 
-<<<<<<< HEAD
   $("#Desktop span").click(function(e) {
     e.stopPropagation();
     if (e.shiftKey) {
@@ -76,32 +74,6 @@ $(function() {
     $('#trash').css('display', 'none');
   });
 
-  //Background Color
-  // class Setting {
-  //     bgColors = ["9BC53D", "5BC0EB", "E55934", "FDE74C", "F7FFF7"];
-  //     windowColors = ["EBFFFA", "FFCDB2", "FFB4A2", "E5989B", "B5838D"];
-  //
-  //     constructor() {
-  //         this.initSetting("bg");
-  //     }
-  //
-  //     initSetting(type: "bg") {
-  //         const bgContainer = document.getElementById(`${type}-setting`);
-  //         const typeColor = type == "bg" ? this.bgColors;
-  //         typeColor.map((value) => {
-  //             const color = document.createElement("span");
-  //             color.setAttribute("style", `background: #${value};`);
-  //             color.onclick = () => this.changeColor(type, value);
-  //             bgContainer.appendChild(color);
-  //         })
-  //     }
-  //
-  //     changeColor(type: "bg", color: string) {
-  //         document.documentElement.style.setProperty(type === "bg" ? "--main-color", `#${color}`);
-  //     }
-  //
-  // }
-
   $("#er").click(function() {
     $('#err').css('display', 'block');
   });
@@ -109,10 +81,6 @@ $(function() {
   $(".cberr").click(function() {
     $('#err').css('display', 'none');
   });
-
-  // $( ".article-icon" ).click(function() {
-  //   $(#article_ + article_id.css('display','block');
-  // });
 
   // попап в статье
   $('#popup-close').on('click', function() {
@@ -122,6 +90,11 @@ $(function() {
   $(".popup").draggable();
 
   $("#gui").click(function() {
+    $('#guiewrise').css('display', 'block');
+    $('#firstPage').css('display', 'block');
+  });
+
+  $("#fastlink_erwise").click(function() {
     $('#guiewrise').css('display', 'block');
     $('#firstPage').css('display', 'block');
   });
@@ -146,173 +119,17 @@ $(function() {
   $("#mailb").click(function() {
     $('#mailb.foldercontent').css('display', 'block');
   });
-  // // $( ".cberr" ).click(function() {
-  // // $('#err').css('display','none');
-  // });
 
-});
-
-function setImgPosition() {
-  $('.massive_article').each(function() {
-    var randPosX = Math.ceil((Math.random() * maxPosX)),
-      randPosY = Math.ceil((Math.random() * maxPosY)),
-      randZind = Math.ceil((Math.random() * 3));
-    $('.article_main').css({
-      'left': randPosX,
-      'top': randPosY,
-      'z-index': randZind
-    });
+  $("#att").click(function() {
+    $('#landing').css('display', 'block');
   });
-}
-window.onload = setImgPosition;
 
-// New position button
-$('.new-pos').click(function(e) {
-  e.preventDefault();
-  setImgPosition();
-});
-=======
-//MENUBAR
-$("#Desktop span").click(function(e) {
-		e.stopPropagation();
-		if (e.shiftKey) {
-				//Shift-Click
-				$(this).addClass("focus");
-		} else {
-				$(".focus").removeClass("focus");
-				$(this).addClass("focus");
-		}
-});
-$("body:not(#Desktop span)").click(function() {
-		$("#Desktop span").removeClass("focus");
-
- $("#Desktop span").click(function(e) {
-	 	e.stopPropagation();
-	 	if (e.shiftKey) {
-	 			//Shift-Click
-	 			$(this).addClass("focus");
-	 	} else {
-	 			$(".focus").removeClass("focus");
-	 			$(this).addClass("focus");
-	 	}
+  $("#fastlink_att").click(function() {
+    $('#landing').css('display', 'block');
   });
- $("body:not(#Desktop span)").click(function() {
- 		$("#Desktop span").removeClass("focus");
+
+  $(".cberr").click(function() {
+    $('#landing').css('display', 'none');
   });
+  
 });
-
-//FOLDERS
-$( "#infofolder" ).dblclick(function() {
-  $('#info').css('display','block');
-});
-
- $( "#computerfolder" ).dblclick(function() {
-   $('#computer').css('display','block');
- });
-
- $( "#folderfolder" ).dblclick(function() {
-   $('#folder').css('display','block');
- });
-
- $( "#trashfolder" ).dblclick(function() {
-   $('#trash').css('display','block');
- });
-
-
- $( ".cbinfo" ).click(function() {
-   $('#info').css('display','none');
- });
-
- $( ".cbcomputer" ).click(function() {
-   $('#computer').css('display','none');
- });
-
- $( ".cbfolder" ).click(function() {
-   $('#folder').css('display','none');
- });
-
- $( ".cberr" ).click(function() {
-   $('#trash').css('display','none');
- });
-
-
-//Background Color
-// class Setting {
-//     bgColors = ["9BC53D", "5BC0EB", "E55934", "FDE74C", "F7FFF7"];
-//     windowColors = ["EBFFFA", "FFCDB2", "FFB4A2", "E5989B", "B5838D"];
-//
-//     constructor() {
-//         this.initSetting("bg");
-//     }
-//
-//     initSetting(type: "bg") {
-//         const bgContainer = document.getElementById(`${type}-setting`);
-//         const typeColor = type == "bg" ? this.bgColors;
-//         typeColor.map((value) => {
-//             const color = document.createElement("span");
-//             color.setAttribute("style", `background: #${value};`);
-//             color.onclick = () => this.changeColor(type, value);
-//             bgContainer.appendChild(color);
-//         })
-//     }
-//
-//     changeColor(type: "bg", color: string) {
-//         document.documentElement.style.setProperty(type === "bg" ? "--main-color", `#${color}`);
-//     }
-//
-// }
-
-$( "#er" ).click(function() {
-  $('#err').css('display','block');
-});
-
-$( ".cberr" ).click(function() {
-  $('#err').css('display','none');
-});
-
-$( ".article-icon" ).click(function() {
-   $('#err').css('display','block');
- });
-
-// $( ".article-icon" ).click(function() {
-//   $(#article_ + article_id.css('display','block');
-// });
-
-// попап в статье
-$('#popup-close').on('click', function(){
-  $('.popup').toggleClass('isClosed')
-});
-
-$( function() {
-  $( ".popup" ).draggable();
-});
-
-$( "#gui" ).click(function() {
-  $('#guiewrise').css('display','block');
-  $('#firstPage').css('display','block');
-});
-
-$( "#close" ).click(function() {
-  $('#guiewrise').css('display','none');
-});
-
-$( ".next" ).click(function() {
-  if ('#firstPage') {
-    $('#firstPage').css('display','none');
-    $('#secondPage').css('display','block');
-  } else if ('#secondPage') {
-    $('#secondPage').css('display','none');
-    $('#thirdPage').css('display','block');
-  } else {
-    $('#secondPage').css('display','none');
-    $('#firstPage').css('display','none');
-  }
-});
-
-$( "#mailb" ).click(function() {
-  $('#mailb').css('display','block');
-});
-  // // $( ".cberr" ).click(function() {
-  // // $('#err').css('display','none');
-  // });
->>>>>>> d3de11b225a71fc4930756a26356a779443fe11a
--- app/models/folderfolders.rb
+++ app/models/folderfolders.rb
@@ -1,8 +1,8 @@
 class Folderfolder < ApplicationRecord
-<<<<<<< HEAD:app/models/folderfolder.rb
+:app/models/folderfolder.rb
     #has_many :articles
 end
 =======
-     has_many :articles
+     #has_many :articles
  end
->>>>>>> d3de11b225a71fc4930756a26356a779443fe11a:app/models/folderfolders.rb
+:app/models/folderfolders.rb
