d42261bbf890f651d5c763026dd500779cbd0fa9
Oxana Kochueva <oxanak99@gmail.com>
2018-12-18 15:16:19 +0300

    assets fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,15 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'att', 'gif')
+# Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'erwise', 'png')
+# Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'icons', 'png')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'att')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'erwise')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'icons')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+# Rails.application.config.assets.precompile += %w( stylesheet.css, att/*, erwise/*, icons/* )
 Rails.application.config.assets.precompile += %w( stylesheet.css )
