02697c20f1f4e3c8962103175f2139ad9b85acc7
Oxana Kochueva <oxanak99@gmail.com>
2018-12-19 01:10:02 +0300

    try2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,4 +18,4 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'image
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( stylesheet.css, att/*, erwise/*, icons/* )
-Rails.application.config.assets.precompile += %w( stylesheet.css )
+Rails.application.config.assets.precompile += %w( stylesheet.css, articles.scss )
