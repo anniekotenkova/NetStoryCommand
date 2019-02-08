da3385d6c7ebfaba8e1318808b72fe39864835a2
OlShum <olya.shumilova@mail.ru>
2019-02-08 17:07:09 +0300

    начало сидов

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 # require 'faker'
-#
-# # Reset database
+
+# Reset database
 # Rake::Task['db:drop'].invoke
 # Rake::Task['db:create'].invoke
 # Rake::Task['db:migrate'].invoke
 #
-# def create_artciles
-#   3.times do
-#     create_artcile
-#   end
+# def seed_data
+#   create_folder1960
 # end
 #
-# def create_artcile
-#   post = Post.create
+# def create_folder1960
+#   folder = Folder.create(
+#     name: "1960"
+#     icon: File.open(File.join(Rails.root, "assets/images/folder.png"))
+#   )
 # end
