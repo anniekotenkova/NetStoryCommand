e320fbcad6a709b2f23a9b8802e15e9a63145410
anniekotenkova <31893553+anniekotenkova@users.noreply.github.com>
2019-02-10 20:46:45 +0300

    Architecture 1.1

---
+++ app/assets/javascripts/spams.js
@@ -0,0 +1,48 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+(function(){
+    var pornLinks = document.querySelectorAll('[href="#porn"]'),
+        pornWindows = document.querySelectorAll('.porn-window'),
+        pornContainer = document.querySelector('.porn');
+
+    var i;
+
+    var delay = 900,
+        called = false;
+
+    function delayWindowOpen(el, time){
+        setTimeout(function(){
+            el.classList.remove('hidden');
+        }, time)
+    }
+
+    function animatePornWindows(){
+        for(i = 1; i < pornWindows.length; i++){
+            var time = delay * i;
+            delayWindowOpen(pornWindows[i], time);
+        }
+    }
+
+    for(i = 0; i < pornLinks.length; i++){
+        pornLinks[i].addEventListener('click', function(){
+            setTimeout(function(){
+                if(called == false){
+                    document.querySelector('#porn1').classList.remove('hidden');
+                    called = true;
+                    animatePornWindows();
+                }
+            }, 2000);
+        });
+    }
+
+    pornContainer.addEventListener('click', function(){
+        if(pornContainer.querySelector('.hidden')){
+            called = false;
+            for(i = 1; i < pornWindows.length; i++){
+                pornWindows[i].classList.remove('.hidden');
+            }
+        }
+    });
+})();
--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_02_04_142406) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
   end
 
   create_table "years", force: :cascade do |t|
