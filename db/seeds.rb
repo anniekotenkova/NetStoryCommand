7e4032d8e36926648ad3a75b505dff9b9d8552ee
OlShum <olya.shumilova@mail.ru>
2019-02-26 23:47:31 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ end
 def create_folders
 
   Folder.create(name: 'Справка', icon: 'Spravka.svg')
+  
   computer = Folder.create(name: 'Мой компьютер', icon: 'computer.png')
-
 
   folder = Folder.find_by_name('Мой компьютер')
   computer.folders.create(name: '1970', icon: 'folder.svg')
@@ -24,6 +24,8 @@ def create_folders
 
   Folder.create(name: 'Корзина', icon: 'Korzina.png')
   Folder.create(name: 'Интернет', icon: 'internet.svg')
+
+  puts "Folder with name #{ folder.name } and icon #{ folder.icon } was created"
 end
 
 def create_articles
@@ -33,13 +35,15 @@ def create_articles
   folder = Folder.find_by_name('1980')
 
   folder = Folder.find_by_name('1990')
-  folder.articles.create(title: 'Ошибка 404',     pic: File.open(File.join(Rails.root, 'app/assets/images/icons/notfound_icon.png')))
-  folder.articles.create(title: 'Первый браузер', pic: File.open(File.join(Rails.root, 'app/assets/images/icons/erwise_icon.png')))
-  folder.articles.create(title: 'Первый лендинг', pic: File.open(File.join(Rails.root, 'app/assets/images/icons/at&t_icon.png')))
-  folder.articles.create(title: 'Друзья',         pic: File.open(File.join(Rails.root, 'app/assets/images/icons/at&t_icon.png')))
+  folder.articles.create(title: 'Ошибка 404',     pic: 'notfound_icon.png')
+  folder.articles.create(title: 'Первый браузер', pic: 'erwise_icon.png')
+  folder.articles.create(title: 'Первый лендинг', pic: 'at&t_icon.png')
+  folder.articles.create(title: 'Друзья',         pic: 'at&t_icon.png')
 
   folder = Folder.find_by_name('2000')
   folder.articles.create(title: 'Википедия',      pic: File.open(File.join(Rails.root, 'app/assets/images/icons/at&t_icon.png')))
+
+  puts "Folder with title #{ article.title } was created"
 end
 
 data_seed
