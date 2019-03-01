2fb19edcc1dcb241b03a792dfe94534279c149cc
Oxana Kochueva <oxanak99@gmail.com>
2019-03-01 08:12:21 +0300

    исправление ошибок, сид для новой статьи

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,7 @@ def create_articles
   folder.articles.create(title: 'Первая почта',   pic: 'mail_icon.png',      partial_name: 'email')
 
   folder = Folder.find_by_name('1980')
-  folder.articles.create(title: 'NSFNET', pic: 'mail_icon.png')
+  folder.articles.create(title: 'NSFNET', pic: 'mail_icon.png', partial_name: 'nsfnet')
 
   folder = Folder.find_by_name('1990')
   folder.articles.create(title: 'Ошибка 404',     pic: 'notfound_icon.png', partial_name: 'error404')
