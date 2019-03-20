470f02092c720d48f9ac6b8e57ad5c8b26dde5f6
Oxana Kochueva <oxanak99@gmail.com>
2019-03-20 12:38:31 +0300

    пыталась починить фильтры, но не вышло
    зато два файла лишних удалила

--- app/assets/javascripts/menuhidden.js
+++ app/assets/javascripts/menuhidden.js
@@ -1,35 +1,33 @@
-$(function() {
 
-  startTime();
-  $('#setting').hide();
+$(function() {
+  var dropDown = document.querySelector('.setting__dropdown'),
+  dropDownButton = document.querySelector('.setting__dropdown-button');
+
+  startTime()
+  $('#setting').hide()
   $('#start').click(function() {
-    $('#setting').toggle();
-    $(this).toggleClass('startClick');
-  });
+    $('#setting').toggle()
+    $(this).toggleClass('startClick')
+  })
 
   $('.setting__dropdown-button').click(function() {
     var dropDown = document.querySelector('.setting__dropdown');
     if (dropDown.className.indexOf('hidden') >= 0) {
-      dropDown.classList.remove('hidden');
+      dropDown.classList.remove('hidden')
     } else {
-      dropDown.classList.add('hidden');
+      dropDown.classList.add('hidden')
     }
-  });
+  })
 
   $('.setting__dropdown__option').click(function(e) {
-    console.log('Filter');
-    e.preventDefault();
+    console.log('Filter')
+    e.preventDefault()
 
-    var dropDown = document.querySelector('.setting__dropdown'),
-      dropDownButton = document.querySelector('.setting__dropdown-button');
-
-    dropDown.classList.add('hidden');
+    dropDown.classList.add('hidden')
     dropDownButton.textContent = e.target.textContent;
 
     document.querySelector('body').className = 'body-overflow-h' + ' ' + e.target.id;
-  });
+    console.log('Fil')
+  })
 
-  var dropDown = document.querySelector('.setting__dropdown'),
-  dropDownButton = document.querySelector('.setting__dropdown-button');
-
-});
+})
