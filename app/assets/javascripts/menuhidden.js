5ab1f458747667b8680fb10225b689a148a8a030
Oxana Kochueva <oxanak99@gmail.com>
2019-03-20 08:50:47 +0300

    фон

--- app/assets/javascripts/menuhidden.js
+++ app/assets/javascripts/menuhidden.js
@@ -1,4 +1,22 @@
 $(function() {
+
+  $("#pink").click(function() {
+    $('body').css('background', '#FF85A6');
+    console.log(pink);
+  });
+
+  $("#blue").click(function() {
+    $('body').css('background', '#2196F3');
+  });
+
+  $("#yellow").click(function() {
+    $('body').css('background', '#FEC006');
+  });
+
+  $("#black").click(function() {
+    $('body').css('background', '#1B1725');
+  });
+
   startTime();
   $('#setting').hide();
   $('#start').click(function() {
