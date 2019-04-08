59497d758ffaa10cea15ffc26386c3cfd4b6ae42
Oxana Kochueva <oxanak99@gmail.com>
2019-04-08 13:18:22 +0300

    стили спама и исправление косяков

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,20 +1,23 @@
 $(function() {
-  
+
   // background color change
   $("#pink").click(function() {
-    $('#Desktop').css('background', '#FF85A6');
+    $('#Desktop').css('background', '#FF85A6')
+    $('.folderfolder a').removeClass('black')
   })
 
   $("#blue").click(function() {
-    $('#Desktop').css('background', '#2196F3');
+    $('#Desktop').css('background', '#2196F3')
+    $('.folderfolder a').removeClass('black')
   })
 
   $("#yellow").click(function() {
-    $('#Desktop').css('background', '#FEC006');
+    $('#Desktop').css('background', '#FEC006')
+    $('.folderfolder a').removeClass('black')
   })
 
   $("#black").click(function() {
-    $('#Desktop').css('background', '#1B1725');
+    $('#Desktop').css('background', '#1B1725')
     $('.folderfolder a').addClass('black')
   })
 })
