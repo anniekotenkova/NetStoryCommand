bf692404a5580fc42744ccac882aa85911c3370c
Oxana Kochueva <oxanak99@gmail.com>
2019-05-18 14:39:05 +0300

    УДАЛЯЕТСЯ ТОЛЬКО КОМП ЕЕЕЕЕЕЕ

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -1,5 +1,10 @@
 $(window).on("load", function() {
   document.getElementsByTagName('body')[0].dataset.layer = 1
   $(".draggable").draggable()
-  $("#containerFolder_9").droppable()
+  $("#containerFolder_9").droppable({
+    accept: "#containerFolder_2",
+    drop: function(event, ui) {
+      $('.screenBack').removeClass('hidden')
+    }
+  })
 })
