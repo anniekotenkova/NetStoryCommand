7d0f6699bf66f6042955736db5c8263f70f7b829
ZakharDay <zakhar.day@gmail.com>
2019-05-31 13:13:30 +0300

    Work snapshot

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -1,6 +1,20 @@
 $(window).on("load", function() {
   document.getElementsByTagName('body')[0].dataset.layer = 1
-  $(".draggable").draggable()
+  var layer = parseInt(document.getElementsByTagName('body')[0].dataset.layer)
+
+  $(".draggable").draggable({
+    // var self = this
+    // zIndex: layer,
+    start: function() {
+      layer = parseInt(document.getElementsByTagName('body')[0].dataset.layer) + 1
+      document.getElementsByTagName('body')[0].dataset.layer = layer
+      $(this).css("z-index", layer)
+    },
+    stop: function() {
+      $(this).css("z-index", layer)
+    }
+  })
+
   $("#containerFolder_9").droppable({
     accept: "#containerFolder_2",
     drop: function(event, ui) {
