fb45c82515f5cd0b974e8b740b4bdc43e285b585
Oxana Kochueva <oxanak99@gmail.com>
2019-06-06 19:34:17 +0300

    заглушка на комп, отправка почты ещё не работает, но я в процессе понимания
    пы сы
    не пугайтесь, если написать в строке /folders вы будете на привычной странице :3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :welcomes
   get 'welcome/index'
 
   resources :folders do
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   resources :articles
 
 
-  root 'folders#index'
+  #root 'folders#index'
+  root 'welcomes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_102110) do
+ActiveRecord::Schema.define(version: 2019_06_06_120525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,11 @@ ActiveRecord::Schema.define(version: 2019_05_16_102110) do
 
   create_table "spams", force: :cascade do |t|
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
