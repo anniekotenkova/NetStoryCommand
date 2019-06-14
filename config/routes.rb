a1b1347a3635954989e7c3bd5ee9ff490100985e
Oxana Kochueva <oxanak99@gmail.com>
2019-06-14 18:36:04 +0300

    картинка в трёх размерах

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   scope "/:locale", locale: /ru|en/ do
-    #resources :spams
-    #resources :articles
-#
-    #resources :folders do
-    #  member do
-    #    get :back
-    #  end
-    #end
+      resources :spams
+      resources :articles
+
+      resources :folders do
+        member do
+          get :back
+        end
+      end
 
     resources :subscriptions, only: [:new, :create]
 
