c5064b75c5337cfdaa45ff2f6a00fb31488ab2ff
Oxana Kochueva <oxanak99@gmail.com>
2019-06-14 12:56:29 +0300

    удаление лишнего

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   scope "/:locale", locale: /ru|en/ do
-    # resources :spams
-    # resources :articles
-    #
-    # resources :folders do
-    #   member do
-    #     get :back
-    #   end
-    # end
+    #resources :spams
+    #resources :articles
+#
+    #resources :folders do
+    #  member do
+    #    get :back
+    #  end
+    #end
 
     resources :subscriptions, only: [:new, :create]
 
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
     root 'subscriptions#new'
   end
 
+  #root 'folders#index'
   root 'subscriptions#new'
 end
