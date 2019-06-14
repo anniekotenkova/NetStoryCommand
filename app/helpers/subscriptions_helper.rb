9eac7cb92e1fabd9254efda6132826c50c9e6b7a
Oxana Kochueva <oxanak99@gmail.com>
2019-06-14 18:37:10 +0300

    лишнее

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,8 +1,3 @@
 module SubscriptionsHelper
 
-#    def image_set_tag(source, srcset = {}, options = {})
-#    srcset = srcset.map { |src, size| "#{path_to_image(src)} #{size}" }.join(', ')
-#    image_tag(source, options.merge(srcset: srcset))
-#  end
-
 end
